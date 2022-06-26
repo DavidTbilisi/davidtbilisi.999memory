e2b90d022eb2cc0a04d71bfc7dd00ee66a2f046e
David <davidchincharashvili@gmail.com>
2022-06-26 08:32:11 +0400

    added pao till 30

--- index.js
+++ index.js
@@ -4996,7 +4996,708 @@ let data_arr = [
     }
 ]
 
-
+let pao = [
+    {
+        "NUMBER": "0",
+        "CATEGORY": "Princess Mononoke",
+        "PERSON": "ტყის სული",
+        "ACTION": "იძრობს",
+        "OBJECT": "თავს"
+    },
+    {
+        "NUMBER": "1",
+        "CATEGORY": "Princess Mononoke",
+        "PERSON": "ნაგო",
+        "ACTION": "დასდევს",
+        "OBJECT": "ირემს"
+    },
+    {
+        "NUMBER": "2",
+        "CATEGORY": "Princess Mononoke",
+        "PERSON": "ოკოტო",
+        "ACTION": "შურს ძიობს",
+        "OBJECT": "მეგობრის გამო"
+    },
+    {
+        "NUMBER": "3",
+        "CATEGORY": "Princess Mononoke",
+        "PERSON": "მორო",
+        "ACTION": "წევს",
+        "OBJECT": "კლდეზე"
+    },
+    {
+        "NUMBER": "4",
+        "CATEGORY": "Princess Mononoke",
+        "PERSON": "კოდამები",
+        "ACTION": "მხიარულობენ",
+        "OBJECT": "ხეზე"
+    },
+    {
+        "NUMBER": "5",
+        "CATEGORY": "Princess Mononoke",
+        "PERSON": "სანი",
+        "ACTION": "მგზავრობს",
+        "OBJECT": "მგელზე"
+    },
+    {
+        "NUMBER": "6",
+        "CATEGORY": "Princess Mononoke",
+        "PERSON": "აშუტაჯა",
+        "ACTION": "იცავს",
+        "OBJECT": "გოგონას"
+    },
+    {
+        "NUMBER": "7",
+        "CATEGORY": "Princess Mononoke",
+        "PERSON": "ებოში",
+        "ACTION": "დევნის",
+        "OBJECT": "ცხოველებს"
+    },
+    {
+        "NUMBER": "8",
+        "CATEGORY": "Princess Mononoke",
+        "PERSON": "ჯოგ",
+        "ACTION": "ნადირობს",
+        "OBJECT": "ტყეში"
+    },
+    {
+        "NUMBER": "9",
+        "CATEGORY": "Princess Mononoke",
+        "PERSON": "გონზას",
+        "ACTION": "უღუნავენ",
+        "OBJECT": "ხმალს"
+    },
+    {
+        "NUMBER": "10",
+        "CATEGORY": "Princess Mononoke",
+        "PERSON": "იაკული",
+        "ACTION": "გაურბის",
+        "OBJECT": "ტახს"
+    },
+    {
+        "NUMBER": "11",
+        "CATEGORY": "Death Note",
+        "PERSON": "რიუკი",
+        "ACTION": "აგდებს",
+        "OBJECT": "რვეულს"
+    },
+    {
+        "NUMBER": "12",
+        "CATEGORY": "Death Note",
+        "PERSON": "რემი",
+        "ACTION": "თავს სწირავს",
+        "OBJECT": "ადამიანისთვის"
+    },
+    {
+        "NUMBER": "13",
+        "CATEGORY": "Death Note",
+        "PERSON": "ვატარი",
+        "ACTION": "ატარებს",
+        "OBJECT": "პრეზენტაციას"
+    },
+    {
+        "NUMBER": "14",
+        "CATEGORY": "Death Note",
+        "PERSON": "ელი",
+        "ACTION": "ყრის",
+        "OBJECT": "ფულს"
+    },
+    {
+        "NUMBER": "15",
+        "CATEGORY": "Death Note",
+        "PERSON": "ნიარი",
+        "ACTION": "ეთამაშება",
+        "OBJECT": "თმას"
+    },
+    {
+        "NUMBER": "16",
+        "CATEGORY": "Death Note",
+        "PERSON": "მელოუ",
+        "ACTION": "ყიდულობს",
+        "OBJECT": "შოკოლადს"
+    },
+    {
+        "NUMBER": "17",
+        "CATEGORY": "Death Note",
+        "PERSON": "ლაითი",
+        "ACTION": "ჭამს",
+        "OBJECT": "ჩიფსებს"
+    },
+    {
+        "NUMBER": "18",
+        "CATEGORY": "Death Note",
+        "PERSON": "მისა",
+        "ACTION": "არიგებს",
+        "OBJECT": "ავტოგრაფებს"
+    },
+    {
+        "NUMBER": "19",
+        "CATEGORY": "Death Note",
+        "PERSON": "რეი პემბერი",
+        "ACTION": "ამშვიდებს",
+        "OBJECT": "საცოლეს"
+    },
+    {
+        "NUMBER": "20",
+        "CATEGORY": "Death Note",
+        "PERSON": "ნაომი",
+        "ACTION": "იგვიანებს",
+        "OBJECT": "პაემანზე"
+    },
+    {
+        "NUMBER": "21",
+        "CATEGORY": "Naruto",
+        "PERSON": "ჯერაია",
+        "ACTION": "წერს",
+        "OBJECT": "წიგნს"
+    },
+    {
+        "NUMBER": "22",
+        "CATEGORY": "Naruto",
+        "PERSON": "ოროჩიმარუ",
+        "ACTION": "დგამს",
+        "OBJECT": "ცდებს"
+    },
+    {
+        "NUMBER": "23",
+        "CATEGORY": "Naruto",
+        "PERSON": "ცუნადე",
+        "ACTION": "სვამს",
+        "OBJECT": "საკეს"
+    },
+    {
+        "NUMBER": "24",
+        "CATEGORY": "Naruto",
+        "PERSON": "ნარუტო",
+        "ACTION": "ატარებს მოლაპარაკებას",
+        "OBJECT": "მელასთან"
+    },
+    {
+        "NUMBER": "25",
+        "CATEGORY": "Naruto",
+        "PERSON": "სასკე",
+        "ACTION": "იმორჩილებს",
+        "OBJECT": "გველს"
+    },
+    {
+        "NUMBER": "26",
+        "CATEGORY": "Naruto",
+        "PERSON": "საკურა",
+        "ACTION": "კურნავს",
+        "OBJECT": "ლოკოკინას"
+    },
+    {
+        "NUMBER": "27",
+        "CATEGORY": "Naruto",
+        "PERSON": "ჰინატა",
+        "ACTION": "ხედავს",
+        "OBJECT": "ჩაკრას"
+    },
+    {
+        "NUMBER": "28",
+        "CATEGORY": "Naruto",
+        "PERSON": "კიბა",
+        "ACTION": "იცავს",
+        "OBJECT": "ცხვირს"
+    },
+    {
+        "NUMBER": "29",
+        "CATEGORY": "Naruto",
+        "PERSON": "შინო",
+        "ACTION": "ამრავლებს",
+        "OBJECT": "ხოჭოებს"
+    },
+    {
+        "NUMBER": "30",
+        "CATEGORY": "Naruto",
+        "PERSON": "შირამარუ",
+        "ACTION": "უშვებს",
+        "OBJECT": "ჩრდილს"
+    },
+    {
+        "NUMBER": "31",
+        "CATEGORY": "Avatar",
+        "PERSON": "ავატარი",
+        "ACTION": "ურთიერთობს",
+        "OBJECT": "სულებთან"
+    },
+    {
+        "NUMBER": "32",
+        "CATEGORY": "Avatar",
+        "PERSON": "აპპას",
+        "ACTION": "სცვივა",
+        "OBJECT": "ბეწვი"
+    },
+    {
+        "NUMBER": "33",
+        "CATEGORY": "Avatar",
+        "PERSON": "მომო",
+        "ACTION": "იწმენდს",
+        "OBJECT": "ყურებს"
+    },
+    {
+        "NUMBER": "34",
+        "CATEGORY": "Avatar",
+        "PERSON": "ბუმი",
+        "ACTION": "სრიალებს",
+        "OBJECT": "სასრიალოზე"
+    },
+    {
+        "NUMBER": "35",
+        "CATEGORY": "Avatar",
+        "PERSON": "ტოფი",
+        "ACTION": "იმარჯვებს",
+        "OBJECT": "შეჯიბრში"
+    },
+    {
+        "NUMBER": "36",
+        "CATEGORY": "Avatar",
+        "PERSON": "დაილი",
+        "ACTION": "ადებენ ბორიკლებს",
+        "OBJECT": "მძევლებს"
+    },
+    {
+        "NUMBER": "37",
+        "CATEGORY": "Avatar",
+        "PERSON": "ზუკო",
+        "ACTION": "წვავს",
+        "OBJECT": "საჭმელს"
+    },
+    {
+        "NUMBER": "38",
+        "CATEGORY": "Avatar",
+        "PERSON": "აზულა",
+        "ACTION": "აბავს",
+        "OBJECT": "ბიჭებს"
+    },
+    {
+        "NUMBER": "39",
+        "CATEGORY": "Avatar",
+        "PERSON": "აირო",
+        "ACTION": "იღებს",
+        "OBJECT": "აბაზანას"
+    },
+    {
+        "NUMBER": "40",
+        "CATEGORY": "Avatar",
+        "PERSON": "კატარა",
+        "ACTION": "რეცხავს",
+        "OBJECT": "სარეცხს"
+    },
+    {
+        "NUMBER": "41",
+        "CATEGORY": "Mortal Combat",
+        "PERSON": "რეიდენი",
+        "ACTION": "ისვრის",
+        "OBJECT": "ელვებს"
+    },
+    {
+        "NUMBER": "42",
+        "CATEGORY": "Mortal Combat",
+        "PERSON": "კუნგლაო",
+        "ACTION": "ლესავს",
+        "OBJECT": "ქუდს"
+    },
+    {
+        "NUMBER": "43",
+        "CATEGORY": "Mortal Combat",
+        "PERSON": "ლიუკინგი",
+        "ACTION": "ათვინიერებს",
+        "OBJECT": "დრაკონს"
+    },
+    {
+        "NUMBER": "44",
+        "CATEGORY": "Mortal Combat",
+        "PERSON": "სკორპიონი",
+        "ACTION": "ამოდის",
+        "OBJECT": "ჯოჯოხეთიდან"
+    },
+    {
+        "NUMBER": "45",
+        "CATEGORY": "Mortal Combat",
+        "PERSON": "საბზირო",
+        "ACTION": "ყინავს",
+        "OBJECT": "ნაყინს"
+    },
+    {
+        "NUMBER": "46",
+        "CATEGORY": "Mortal Combat",
+        "PERSON": "კიტანა",
+        "ACTION": "ინიავებს",
+        "OBJECT": "მარაოთი"
+    },
+    {
+        "NUMBER": "47",
+        "CATEGORY": "Mortal Combat",
+        "PERSON": "მილენა",
+        "ACTION": "ნიღბავს",
+        "OBJECT": "პირს"
+    },
+    {
+        "NUMBER": "48",
+        "CATEGORY": "Mortal Combat",
+        "PERSON": "ჯექსი",
+        "ACTION": "იწრთობს",
+        "OBJECT": "ხელებს"
+    },
+    {
+        "NUMBER": "49",
+        "CATEGORY": "Mortal Combat",
+        "PERSON": "სონია",
+        "ACTION": "აწყობს",
+        "OBJECT": "აქციას"
+    },
+    {
+        "NUMBER": "50",
+        "CATEGORY": "Mortal Combat",
+        "PERSON": "კენიში",
+        "ACTION": "იხვევს",
+        "OBJECT": "თვალებს"
+    },
+    {
+        "NUMBER": "51",
+        "CATEGORY": "Sherlock Holmes",
+        "PERSON": "შერლოკი",
+        "ACTION": "ეწევა",
+        "OBJECT": "ჩიბუხს"
+    },
+    {
+        "NUMBER": "52",
+        "CATEGORY": "Sherlock Holmes",
+        "PERSON": "მაიკროფტი",
+        "ACTION": "დარბის",
+        "OBJECT": "სარბენ ბილიკზე"
+    },
+    {
+        "NUMBER": "53",
+        "CATEGORY": "Sherlock Holmes",
+        "PERSON": "ვოტსონი",
+        "ACTION": "კომპლიმენტს უკეთებს",
+        "OBJECT": "გამვლელებს"
+    },
+    {
+        "NUMBER": "54",
+        "CATEGORY": "Sherlock Holmes",
+        "PERSON": "მერი",
+        "ACTION": "ხვრეტს",
+        "OBJECT": "ხურდას"
+    },
+    {
+        "NUMBER": "55",
+        "CATEGORY": "Sherlock Holmes",
+        "PERSON": "მოლი",
+        "ACTION": "კვეთს",
+        "OBJECT": "გვამს"
+    },
+    {
+        "NUMBER": "56",
+        "CATEGORY": "Sherlock Holmes",
+        "PERSON": "ჰუდსონი",
+        "ACTION": "ამზადებს",
+        "OBJECT": "ჩაის"
+    },
+    {
+        "NUMBER": "57",
+        "CATEGORY": "Sherlock Holmes",
+        "PERSON": "ლესტრეიდი",
+        "ACTION": "მკითხაობს",
+        "OBJECT": "ყავაზე"
+    },
+    {
+        "NUMBER": "58",
+        "CATEGORY": "Sherlock Holmes",
+        "PERSON": "დონოვანი",
+        "ACTION": "ხეხავს",
+        "OBJECT": "იატაკს"
+    },
+    {
+        "NUMBER": "59",
+        "CATEGORY": "Sherlock Holmes",
+        "PERSON": "მორიარტი",
+        "ACTION": "აბრუნებს",
+        "OBJECT": "ვალს"
+    },
+    {
+        "NUMBER": "60",
+        "CATEGORY": "Sherlock Holmes",
+        "PERSON": "ოგუსტუსი",
+        "ACTION": "იმშრალებს",
+        "OBJECT": "ხელებს"
+    },
+    {
+        "NUMBER": "61",
+        "CATEGORY": "Harry Potter",
+        "PERSON": "დამბლდორი",
+        "ACTION": "სვამს",
+        "OBJECT": "მწარე წყალს"
+    },
+    {
+        "NUMBER": "62",
+        "CATEGORY": "Harry Potter",
+        "PERSON": "კოლანდემორტი",
+        "ACTION": "არჩევს",
+        "OBJECT": "ჯოხს"
+    },
+    {
+        "NUMBER": "63",
+        "CATEGORY": "Harry Potter",
+        "PERSON": "სნეიპი",
+        "ACTION": "ასწავლის",
+        "OBJECT": "ბავშვებს"
+    },
+    {
+        "NUMBER": "64",
+        "CATEGORY": "Harry Potter",
+        "PERSON": "ლუპინი",
+        "ACTION": "ემალება",
+        "OBJECT": "მთვარებს"
+    },
+    {
+        "NUMBER": "65",
+        "CATEGORY": "Harry Potter",
+        "PERSON": "სირიუსი",
+        "ACTION": "კვებავს",
+        "OBJECT": "ძაღლებს"
+    },
+    {
+        "NUMBER": "66",
+        "CATEGORY": "Harry Potter",
+        "PERSON": "მინერვა",
+        "ACTION": "ეფერება",
+        "OBJECT": "ფისოს"
+    },
+    {
+        "NUMBER": "67",
+        "CATEGORY": "Harry Potter",
+        "PERSON": "ჰაგრიდი",
+        "ACTION": "ზრდის",
+        "OBJECT": "კვახს"
+    },
+    {
+        "NUMBER": "68",
+        "CATEGORY": "Harry Potter",
+        "PERSON": "პოტერი",
+        "ACTION": "ტეხავს",
+        "OBJECT": "სათვალეს"
+    },
+    {
+        "NUMBER": "69",
+        "CATEGORY": "Harry Potter",
+        "PERSON": "ჰერმიონა",
+        "ACTION": "მოგზაურობს",
+        "OBJECT": "დროში"
+    },
+    {
+        "NUMBER": "70",
+        "CATEGORY": "Harry Potter",
+        "PERSON": "უიზლი",
+        "ACTION": "ხატავს",
+        "OBJECT": "ობობებს"
+    },
+    {
+        "NUMBER": "71",
+        "CATEGORY": "Matrix",
+        "PERSON": "ნეო",
+        "ACTION": "აჩერებს",
+        "OBJECT": "ტყვიებს"
+    },
+    {
+        "NUMBER": "72",
+        "CATEGORY": "Matrix",
+        "PERSON": "მორფეუსს",
+        "ACTION": "არჭობენ",
+        "OBJECT": "ნემსს"
+    },
+    {
+        "NUMBER": "73",
+        "CATEGORY": "Matrix",
+        "PERSON": "ტრინიტი",
+        "ACTION": "კოცნის",
+        "OBJECT": "რჩეულს"
+    },
+    {
+        "NUMBER": "74",
+        "CATEGORY": "Matrix",
+        "PERSON": "ორაკული",
+        "ACTION": "წინასწარმეტყველებს",
+        "OBJECT": "მომავალს"
+    },
+    {
+        "NUMBER": "75",
+        "CATEGORY": "Matrix",
+        "PERSON": "ქალი წითელში",
+        "ACTION": "აწითლებს",
+        "OBJECT": "კაცებს"
+    },
+    {
+        "NUMBER": "76",
+        "CATEGORY": "Matrix",
+        "PERSON": "კოვზის ბიჭი",
+        "ACTION": "ღუნავს",
+        "OBJECT": "კოვზს"
+    },
+    {
+        "NUMBER": "77",
+        "CATEGORY": "Matrix",
+        "PERSON": "სმითი",
+        "ACTION": "გარდაიქმნება",
+        "OBJECT": "ვირუსად"
+    },
+    {
+        "NUMBER": "78",
+        "CATEGORY": "Matrix",
+        "PERSON": "ბრაუნი",
+        "ACTION": "უკვეთავს",
+        "OBJECT": "ნამცხვარს"
+    },
+    {
+        "NUMBER": "79",
+        "CATEGORY": "Matrix",
+        "PERSON": "ჯონსი",
+        "ACTION": "ტენის",
+        "OBJECT": "იარაღს"
+    },
+    {
+        "NUMBER": "80",
+        "CATEGORY": "Matrix",
+        "PERSON": "საიფერი",
+        "ACTION": "კლავს",
+        "OBJECT": "ტანკს"
+    },
+    {
+        "NUMBER": "81",
+        "CATEGORY": "Caribbean Sea",
+        "PERSON": "ჯეკი",
+        "ACTION": "გმირავს",
+        "OBJECT": "გულს"
+    },
+    {
+        "NUMBER": "82",
+        "CATEGORY": "Caribbean Sea",
+        "PERSON": "ედუარდი",
+        "ACTION": "წყვეტს",
+        "OBJECT": "სიმს"
+    },
+    {
+        "NUMBER": "83",
+        "CATEGORY": "Caribbean Sea",
+        "PERSON": "ელიზაბეთი",
+        "ACTION": "იხდის",
+        "OBJECT": "ბრალეტს"
+    },
+    {
+        "NUMBER": "84",
+        "CATEGORY": "Caribbean Sea",
+        "PERSON": "დეივი ჯონზი",
+        "ACTION": "უკრავს",
+        "OBJECT": "ორღანზე"
+    },
+    {
+        "NUMBER": "85",
+        "CATEGORY": "Caribbean Sea",
+        "PERSON": "ბარბოსა",
+        "ACTION": "კმიჩავს",
+        "OBJECT": "ვაშლს"
+    },
+    {
+        "NUMBER": "86",
+        "CATEGORY": "Caribbean Sea",
+        "PERSON": "გიბსს",
+        "ACTION": "სძინავს",
+        "OBJECT": "ღორებთან"
+    },
+    {
+        "NUMBER": "87",
+        "CATEGORY": "Caribbean Sea",
+        "PERSON": "კალიფსო",
+        "ACTION": "აბობოქრებს",
+        "OBJECT": "ოკეანეს"
+    },
+    {
+        "NUMBER": "88",
+        "CATEGORY": "Caribbean Sea",
+        "PERSON": "სუმბაჯი",
+        "ACTION": "ამბობს",
+        "OBJECT": "ჯანდაბას კოდექსი"
+    },
+    {
+        "NUMBER": "89",
+        "CATEGORY": "Caribbean Sea",
+        "PERSON": "ვილი",
+        "ACTION": "ჭედავს",
+        "OBJECT": "ხმალს"
+    },
+    {
+        "NUMBER": "90",
+        "CATEGORY": "Caribbean Sea",
+        "PERSON": "ვილის მამა",
+        "ACTION": "თამაშობს",
+        "OBJECT": "კარტს"
+    },
+    {
+        "NUMBER": "91",
+        "CATEGORY": "GOT",
+        "PERSON": "დეინერისი",
+        "ACTION": "ბანაობს",
+        "OBJECT": "ცეცხლში"
+    },
+    {
+        "NUMBER": "92",
+        "CATEGORY": "GOT",
+        "PERSON": "დრაგო",
+        "ACTION": "ჯირითობს",
+        "OBJECT": "ცხენზე"
+    },
+    {
+        "NUMBER": "93",
+        "CATEGORY": "GOT",
+        "PERSON": "ჯონ სნოუ",
+        "ACTION": "იცვამს",
+        "OBJECT": "შავს"
+    },
+    {
+        "NUMBER": "94",
+        "CATEGORY": "GOT",
+        "PERSON": "სანსა",
+        "ACTION": "დასდევს",
+        "OBJECT": "პრინცს"
+    },
+    {
+        "NUMBER": "95",
+        "CATEGORY": "GOT",
+        "PERSON": "არია",
+        "ACTION": "არჭობს",
+        "OBJECT": "დანას"
+    },
+    {
+        "NUMBER": "96",
+        "CATEGORY": "GOT",
+        "PERSON": "ტირიონი",
+        "ACTION": "ხუმრობს",
+        "OBJECT": "ჯუჯებზე"
+    },
+    {
+        "NUMBER": "97",
+        "CATEGORY": "GOT",
+        "PERSON": "სერსეი",
+        "ACTION": "ვნებიანად უყურებს",
+        "OBJECT": "ძმას"
+    },
+    {
+        "NUMBER": "98",
+        "CATEGORY": "GOT",
+        "PERSON": "ვარისი",
+        "ACTION": "შედის",
+        "OBJECT": "ტუალეტში"
+    },
+    {
+        "NUMBER": "99",
+        "CATEGORY": "GOT",
+        "PERSON": "ბეილიში",
+        "ACTION": "აფრენს",
+        "OBJECT": "ფრინველებს"
+    }
+]
 new Vue({
     el: "#app",
     data: {
